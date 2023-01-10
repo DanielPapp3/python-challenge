dc9a9053c5e10622f45136e67bba2fc481f3a278
DanielPapp3 <dapapp3@gmail.com>
2023-01-09 21:14:11 -0600

    Added comments to python files.

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,8 +16,10 @@ financial_csv = os.path.join("Resources", "budget_data.csv")
 with open(financial_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    # Stores the header.
     header = next(csvreader)
 
+    # Variable to store the value of the previous row's profit. Used to calculate monthly changes in profits.
     LastRow = 0
 
     for row in csvreader:
@@ -26,10 +28,12 @@ with open(financial_csv) as csvfile:
         Monthly_Change.append(int(row[1]) - int(LastRow))
         DateList.append(row[0])
 
+        # Updates LastRow value before moving onto next row in loop.
         LastRow = row[1]
     
     Avg_Change = round((sum(Monthly_Change) - Monthly_Change[0]) / (Months - 1),2)
 
+    # Sets the greatest increase/decrease amounts and the accompanying months during which those records were set.
     Greatest_Increase_Amt = max(Monthly_Change)
     Greatest_Increase_Date = DateList[Monthly_Change.index(max(Monthly_Change))]
     Greatest_Decrease_Amt = min(Monthly_Change)
@@ -37,7 +41,9 @@ with open(financial_csv) as csvfile:
 
     output = f"Financial Analysis\n----------------------------\nTotal Months: {Months}\nTotal: ${Profit}\nAverage Change: ${Avg_Change}\nGreatest Increase in Profits: {Greatest_Increase_Date} (${Greatest_Increase_Amt})\nGreatest Decrease in Profits: {Greatest_Decrease_Date} (${Greatest_Decrease_Amt})"
 
+    # Prints results to the terminal.
     print(output)
 
+    # Exports the results to a text file.
     with open('Analysis\Financial_Analysis.txt', 'w') as file:
         file.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,14 +13,17 @@ electoral_csv = os.path.join("Resources", "election_data.csv")
 with open(electoral_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    # Stores the header.
     header = next(csvreader)
 
     for row in csvreader:
         Votes += 1
 
+        # Checks if a vote is for a new candidate. If it is, add the new candidate to the Candidates list.
         if row[2] not in Candidates:
             Candidates.append(row[2])
         
+        # Updates vote totals for the appropriate candidate.
         if row[2] == "Charles Casper Stockham":
             Vote_Total_Charles += 1
         elif row[2] == "Diana DeGette":
@@ -28,6 +31,7 @@ with open(electoral_csv) as csvfile:
         elif row[2] == "Raymon Anthony Doane":
             Vote_Total_Raymon += 1
     
+    # Determine winner of the popular vote by comparing total votes won for each candidate.
     if Vote_Total_Charles > Vote_Total_Diana and Vote_Total_Charles > Vote_Total_Raymon:
         Winner = "Charles Casper Stockham"
     elif Vote_Total_Diana > Vote_Total_Charles and Vote_Total_Diana > Vote_Total_Raymon:
@@ -37,7 +41,9 @@ with open(electoral_csv) as csvfile:
 
     output = f"Election Results\n-------------------------\nTotal Votes: {Votes}\n-------------------------\nCharles Casper Stockham: {round(Vote_Total_Charles/Votes*100,3)}% ({Vote_Total_Charles})\nDiana DeGette: {round(Vote_Total_Diana/Votes*100,3)}% ({Vote_Total_Diana})\nRaymon Anthony Doane: {round(Vote_Total_Raymon/Votes*100,3)}% ({Vote_Total_Raymon})\n-------------------------\nWinner: {Winner}\n-------------------------"
 
+    # Prints the results to the terminal.
     print(output)
 
+    # Exports the results to a text file.
     with open('Analysis\Electoral_Analysis.txt', 'w') as file:
         file.write(output)
